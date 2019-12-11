a950fd17d3060e1b2b6f7987c9648f081ac49bbd
Bruno <bv.santos@campus.fct.unl.pt>
2019-12-11 21:13:22 +0000

    Main spring

---
+++ tp2Srsc/src/main/java/srsc2/tp2Srsc/Main.java
@@ -0,0 +1,15 @@
+package srsc2.tp2Srsc;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(Main.class, args);
+    }
+}
