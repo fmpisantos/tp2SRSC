ab1a2a4ee7a8c98d5b5361199d944c87d7bd0576
FMPI.SANTOS <fmpi.santos@campus.fct.unl.pt>
2019-12-16 17:56:19 +0000

    While

--- Cliente/src/main/java/Main.java
+++ Cliente/src/main/java/Main.java
@@ -23,39 +23,100 @@ public class Main {
     public static void main(String[] args) throws URISyntaxException {
         Scanner in = new Scanner(System.in);
         Menu();
-        int command = in.nextInt();
-        switch(command){
-            case 1:
-                in.nextLine();
-                System.out.println("Enter uuid:");
-                int uuid = in.nextInt();in.nextLine();
-                System.out.println("Other atributes (Enter \"-0\" to stop)");
-                List atts = new ArrayList<String>();
-                String att = in.nextLine();
-                while(!att.equals("-0")){
-                    atts.add(att);
-                    att = in.nextLine();
-                }
-                JsonObject body = new JsonObject();
-                body.addProperty("type", "create");
-                body.addProperty("uuid", uuid);
-                body.addProperty("Attributes",new Gson().toJson(atts));
-                HttpRequest req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI+"/create")).setHeader("Content-type","application/json").build();
-                try {
-                    HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
-                    System.out.println(response.body());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                break;
+        int command = 0;
+        JsonObject body;
+        while(command != -1) {
+            command = in.nextInt();
+            in.nextLine();
+            body = new JsonObject();
+            HttpRequest req;
+            int id;
+            switch (command) {
+                case 1:
+                    System.out.println("Enter uuid:");
+                    int uuid = in.nextInt();
+                    in.nextLine();
+                    System.out.println("Other atributes (Enter \"-0\" to stop)");
+                    List atts = new ArrayList<String>();
+                    String att = in.nextLine();
+                    while (!att.equals("-0")) {
+                        atts.add(att);
+                        att = in.nextLine();
+                    }
+                    body.addProperty("type", "create");
+                    body.addProperty("uuid", uuid);
+                    body.addProperty("Attributes", new Gson().toJson(atts));
+                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/create")).setHeader("Content-type", "application/json").build();
+                    try {
+                        HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+                        System.out.println(response.body());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter user id:");
+                    id = in.nextInt();
+                    in.nextLine();
+                    body.addProperty("type", "list");
+                    body.addProperty("id", id);
+                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/list")).setHeader("Content-type", "application/json").build();
+                    try {
+                        HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+                        System.out.println(response.body());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter user id:");
+                    id = in.nextInt();
+                    in.nextLine();
+                    body.addProperty("type", "all");
+                    body.addProperty("id", id);
+                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/all")).setHeader("Content-type", "application/json").build();
+                    try {
+                        HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+                        System.out.println(response.body());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                    case 5:
+                    System.out.println("Enter your user id:");
+                    id = in.nextInt();
+                    in.nextLine();
+                    System.out.println("Enter your user id:");
+                    int dst = in.nextInt();
+                    in.nextLine();
+                    body.addProperty("type", "send");
+                    body.addProperty("src", id);
+                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/all")).setHeader("Content-type", "application/json").build();
+                    try {
+                        HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+                        System.out.println(response.body());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
         }
     }
 
     public static void Menu(){
         System.out.println("Menu:");
+        System.out.println("-1 - Leave");
         System.out.println("1 - Create Message box");
         System.out.println("2 - List users with a message box");
+        System.out.println("4 - List all messages in a user’s message box");
+        System.out.println("5 - Send a message to a user’s message box");
     }
 }
