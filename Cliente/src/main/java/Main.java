6dd1e761249d583bfdba03822cb35f416a29993b
FMPI.SANTOS <fmpi.santos@campus.fct.unl.pt>
2019-12-16 18:17:00 +0000

    Cliente

--- Cliente/src/main/java/Main.java
+++ Cliente/src/main/java/Main.java
@@ -92,12 +92,38 @@ public class Main {
                     System.out.println("Enter your user id:");
                     id = in.nextInt();
                     in.nextLine();
-                    System.out.println("Enter your user id:");
+                    System.out.println("Enter destination user id:");
                     int dst = in.nextInt();
                     in.nextLine();
+                    String msg = in.nextLine();
+                    String copy = in.nextLine();
                     body.addProperty("type", "send");
                     body.addProperty("src", id);
-                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/all")).setHeader("Content-type", "application/json").build();
+                    body.addProperty("dst",dst);
+                    body.addProperty("msg",msg);
+                    body.addProperty("copy",copy);
+                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/send")).setHeader("Content-type", "application/json").build();
+                    try {
+                        HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+                        System.out.println(response.body());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 7:
+                    System.out.println("Enter your user id:");
+                    id = in.nextInt();
+                    in.nextLine();
+                    msgID = in.nextInt();
+                    in.nextLine();
+                    String receipt = in.nextLine();
+                    body.addProperty("type", "receipt");
+                    body.addProperty("id", id);
+                    body.addProperty("msg",msgID);
+                    body.addProperty("receipt",receipt);
+                    req = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body.toString())).uri(new URI(URI + "/receipt")).setHeader("Content-type", "application/json").build();
                     try {
                         HttpResponse response = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
                         System.out.println(response.body());
@@ -118,5 +144,6 @@ public class Main {
         System.out.println("2 - List users with a message box");
         System.out.println("4 - List all messages in a user’s message box");
         System.out.println("5 - Send a message to a user’s message box");
+        System.out.println("7 - Receipt message sent by a client after receiving and validating a message from a message box");
     }
 }
